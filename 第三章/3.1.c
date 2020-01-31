ad7ed8ed2677033572f25b5721b93ef6cbbb8f8c
helll-art <1321113511@qq.com>
2020-01-31 12:10:50 +0800

    第三章

---
+++ 第三章/3.1.c
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: 3.1.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年01月29日 星期三 21时41分27秒
+ ***********************************************************************/
+ #include<stdio.h>
+ int main()
+ {
+     printf("\aStartled by the sudden sound,Sally shouted,\"By the great Pumpkin,what was that!\"\n");
+     return 0;
+ }
